c7e107cc58e8e26ea9e6b99708aaa4b7ec77d54c
Elias <elias.kohler@aon.at>
2025-04-23 14:21:07 +0200

    feat: add completion provider

---
+++ src/language/bcs-control-lang-completion-provider.ts
@@ -0,0 +1,52 @@
+import { CompletionItemKind } from "vscode-languageserver-types";
+import {
+  DefaultCompletionProvider,
+  CompletionContext,
+  CompletionAcceptor,
+  NextFeature,
+} from "langium/lsp";
+import { isDatapoint, isEnumDecl, isRef } from "./generated/ast.js";
+import type { BCSControlLangServices } from "./bcs-control-lang-module.js";
+import { MaybePromise } from "langium";
+
+export class BCSControlLangCompletionProvider extends DefaultCompletionProvider {
+  constructor(services: BCSControlLangServices) {
+    super(services);
+  }
+
+  protected override completionFor(
+    context: CompletionContext,
+    next: NextFeature,
+    acceptor: CompletionAcceptor
+  ): MaybePromise<void> {
+    const node = context.node;
+
+    if (next.property === "property" && isRef(node)) {
+      const namedElement = node.ref.ref;
+
+      if (isDatapoint(namedElement)) {
+        for (const channel of namedElement.channels) {
+          acceptor(context, {
+            label: channel.name,
+            kind: CompletionItemKind.Field,
+            documentation: `${channel.$container.portgroup.ref?.ioType} Channel of type ${channel.dataType}`,
+          });
+        }
+        return;
+      }
+
+      if (isEnumDecl(namedElement)) {
+        for (const member of namedElement.members) {
+          acceptor(context, {
+            label: member.name,
+            kind: CompletionItemKind.EnumMember,
+            documentation: "Enum member",
+          });
+        }
+        return;
+      }
+    }
+
+    super.completionFor(context, next, acceptor);
+  }
+}
