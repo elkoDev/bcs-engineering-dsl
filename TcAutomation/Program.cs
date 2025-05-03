71c7b470013f36a6d9eba472046cf0bd45e01e46
Elias <elias.kohler@aon.at>
2025-05-03 12:55:20 +0200

    feat: change folder structure

---
+++ TcAutomation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TcAutomation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
