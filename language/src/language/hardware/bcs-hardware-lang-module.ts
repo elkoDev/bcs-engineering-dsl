db6c32321e7e7f8a52bbf79cb8095fdc6f08f70a
Elias <elias.kohler@aon.at>
2025-05-18 14:48:27 +0200

    fix: validating tests

--- language/src/language/hardware/bcs-hardware-lang-module.ts
+++ language/src/language/hardware/bcs-hardware-lang-module.ts
@@ -2,6 +2,7 @@ import type { Module } from "langium";
 import { LangiumServices, PartialLangiumServices } from "langium/lsp";
 import { BCSHardwareLangValidator } from "./bcs-hardware-lang-validator.js";
 import { BCSHardwareLangSemanticTokenProvider } from "./bcs-hardware-lang-semantic-token-provider.js";
+import { BCSHardwareLangScopeProvider } from "./bcs-hardware-lang-scope-provider.js";
 
 export type BCSHardwareAddedServices = {
   validation: {
@@ -16,6 +17,10 @@ export const BCSHardwareLangModule: Module<
   BCSHardwareLangServices,
   PartialLangiumServices & BCSHardwareAddedServices
 > = {
+  references: {
+    ScopeProvider: (services: BCSHardwareLangServices) =>
+      new BCSHardwareLangScopeProvider(services),
+  },
   validation: {
     BCSHardwareLangValidator: () => new BCSHardwareLangValidator(),
   },
